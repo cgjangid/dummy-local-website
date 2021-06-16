c686cca3f240cf031cad20f910847e5ea23da9cd
Chandra <cgjangid@gmail.com>
2021-06-16 17:37:18 +0100

    First Commit

---
+++ js/loginpage.js
@@ -0,0 +1,12 @@
+{
+	function loginSubmit() {
+				
+		if(document.getElementById("exampleInputPassword").value == "password") {
+			window.location.href="index.html";  
+		}
+		else {
+			window.location.href="login.html";
+		}
+	};
+	
+}
